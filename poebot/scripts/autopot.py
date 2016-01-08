965c6f90d6900e0e749e6ab38b614954aa0d2d76
Michael Macmahon <mwmacmahon@gmail.com>
2016-01-07 23:00:19 -0500

    project shell

---
+++ poebot/scripts/autopot.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Runs a basic loop to watch the screen and use potions
+Potion configuration determination method TBD
+
+Created on Thu Jan  7 22:55:58 2016
+
+@author: Michael
+"""
+
+if __name__ == "__main__":
+    pass
